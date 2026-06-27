a6907d748c5299719a09ef8a9e22f5cefdbb17d6
maruyama <taku2000maru@icloud.com>
2026-06-27 22:44:03 +0900

    feat : used_by_userへのmodel記載

--- app/models/organization_invite.rb
+++ app/models/organization_invite.rb
@@ -1,3 +1,7 @@
 class OrganizationInvite < ApplicationRecord
+  # この招待は1つのOrganizationに属する
   belongs_to :organization
+
+  # この招待は1つのUserに属する
+  belongs_to :used_by_user, class_name: 'User', optional: true
 end
