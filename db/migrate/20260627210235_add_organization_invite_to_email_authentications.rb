6f63ab412404790a148cb80c7ca3168257f84f1c
maruyama <taku2000maru@icloud.com>
2026-06-28 06:03:54 +0900

    chore : 生成 20260627210235_add_organization_invite_to_email_authentications

---
+++ db/migrate/20260627210235_add_organization_invite_to_email_authentications.rb
@@ -0,0 +1,5 @@
+class AddOrganizationInviteToEmailAuthentications < ActiveRecord::Migration[8.1]
+  def change
+    add_reference :email_authentications, :organization_invite, null: false, foreign_key: true
+  end
+end
