de5b83328cd1e0777ced6019074ba68009bb351b
maruyama <taku2000maru@icloud.com>
2026-06-27 22:06:21 +0900

    feat : drive_items 再帰構造

--- db/migrate/20260627130304_create_drive_items.rb
+++ db/migrate/20260627130304_create_drive_items.rb
@@ -2,7 +2,7 @@ class CreateDriveItems < ActiveRecord::Migration[8.1]
   def change
     create_table :drive_items do |t|
       t.references :organization, null: false, foreign_key: true
-      t.references :parent, null: false, foreign_key: true
+      t.references :parent, null: true, foreign_key: { to_table: :drive_items }
       t.string :name
       t.integer :item_type
       t.string :blob_path
