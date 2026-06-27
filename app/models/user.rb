ee9569b3e254ec1eae738713cb564081ee51bd8b
maruyama <taku2000maru@icloud.com>
2026-06-28 04:51:13 +0900

    chore : メールを送信できるように環境修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@ class User < ApplicationRecord
   has_many :organization_invites,
            foreign_key: :used_by_user_id,
            dependent: :restrict_with_error
+
+  # このユーザーが作成した招待コード
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 end
