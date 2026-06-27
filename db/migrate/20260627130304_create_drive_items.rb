65f67899154a2670f7be5a0db4c99ff540983b62
maruyama <taku2000maru@icloud.com>
2026-06-27 22:53:01 +0900

    feat : db設計 owner_user作成

--- db/migrate/20260627130304_create_drive_items.rb
+++ db/migrate/20260627130304_create_drive_items.rb
@@ -3,6 +3,9 @@ class CreateDriveItems < ActiveRecord::Migration[8.1]
     create_table :drive_items do |t|
       t.references :organization, null: false, foreign_key: true
       t.references :parent, null: true, foreign_key: { to_table: :drive_items }
+      t.references :owner_user,
+             null: false,
+             foreign_key: { to_table: :users }
       t.string :name
       t.integer :item_type
       t.string :blob_path
