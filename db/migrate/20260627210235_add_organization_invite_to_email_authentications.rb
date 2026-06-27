611e677e8b27cf345b5214449f4de0f90fd8b490
maruyama <taku2000maru@icloud.com>
2026-06-28 06:04:11 +0900

    feat : add_reference email_authentications

--- db/migrate/20260627210235_add_organization_invite_to_email_authentications.rb
+++ db/migrate/20260627210235_add_organization_invite_to_email_authentications.rb
@@ -1,5 +1,5 @@
 class AddOrganizationInviteToEmailAuthentications < ActiveRecord::Migration[8.1]
   def change
-    add_reference :email_authentications, :organization_invite, null: false, foreign_key: true
+    add_reference :email_authentications, :organization_invite, foreign_key: true
   end
 end
