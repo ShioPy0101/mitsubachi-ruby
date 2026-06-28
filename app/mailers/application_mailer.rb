b5583b9014d9816d7d1ed01367c148cdf6148b00
maruyama <taku2000maru@icloud.com>
2026-06-28 16:39:50 +0900

    chore : application_mailer 設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("MAIL_FROM")
-  layout "mailer"
+  default from: mail_from
+
+  private
+
+  def self.mail_from
+    Rails.env.production? ?
+      ENV.fetch("MAIL_FROM") :
+      ENV.fetch("MAIL_FROM", "test@example.com")
+  end
 end
