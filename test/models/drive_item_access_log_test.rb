84670bd2872a95c137d31257ea2550d3db4be4c0
maruyama <taku2000maru@icloud.com>
2026-07-15 22:08:20 +0900

    test: 配信 hardening のテストを追加

--- test/models/drive_item_access_log_test.rb
+++ test/models/drive_item_access_log_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class DriveItemAccessLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "for organization scope filters other organizations" do
+    logs = DriveItemAccessLog.for_organization(organizations(:one))
+
+    assert_includes logs, drive_item_access_logs(:one)
+    assert_not_includes logs, drive_item_access_logs(:two)
+  end
 end
