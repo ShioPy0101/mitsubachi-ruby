cf184feb485891503cef703c93297d37ce8278ee
ShioPy0101 <maruyama.function@gmail.com>
2026-07-23 11:36:10 +0900

    feat: add DriveItem purge state

--- app/models/drive_item.rb
+++ app/models/drive_item.rb
@@ -7,6 +7,7 @@ class DriveItem < ApplicationRecord
 
   # DriveItemは一つのオーナーユーザーに属する
   belongs_to :owner_user, class_name: "User"
+  belongs_to :purged_by_user, class_name: "User", optional: true
 
   # DriveItemは親DriveItemに属することができる（ディレクトリ構造を形成するため）
   belongs_to :parent,
@@ -49,11 +50,11 @@ class DriveItem < ApplicationRecord
 
   # スコープの定義
 
-  # activeなDriveItemを取得するスコープ
-  scope :active, -> { where(deleted_at: nil) }
-
-  # deletedなDriveItemを取得するスコープ
-  scope :deleted, -> { where.not(deleted_at: nil) }
+  scope :active, -> { where(deleted_at: nil, purged_at: nil) }
+  scope :trashed, -> { where.not(deleted_at: nil).where(purged_at: nil) }
+  scope :purged, -> { where.not(purged_at: nil) }
+  scope :not_purged, -> { where(purged_at: nil) }
+  scope :deleted, -> { trashed }
 
   def effective_storage_key
     storage_key.presence || blob_path.presence
